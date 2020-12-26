524344f480f3ea8fbb72492ccead8a9b9d7c3533
panthil malaviya <Toshiba@Panthil_Malaviya>
2020-12-27 00:43:32 +0530

    all fragment file added

---
+++ app/src/main/java/com/rku/blinksales/Settings.java
@@ -0,0 +1,19 @@
+package com.rku.blinksales;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Settings  extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
+    }
+
+}
