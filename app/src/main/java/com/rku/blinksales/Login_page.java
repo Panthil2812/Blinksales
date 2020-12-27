f7d9de02a65d9609a92b06b5ab9757f8c8f7a5bd
panthil malaviya <Toshiba@Panthil_Malaviya>
2020-12-27 22:23:16 +0530

    login and signup page added

---
+++ app/src/main/java/com/rku/blinksales/Login_page.java
@@ -0,0 +1,14 @@
+package com.rku.blinksales;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
--- app/src/main/java/com/rku/blinksales/Signup_page.java
+++ app/src/main/java/com/rku/blinksales/Signup_page.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class registration extends AppCompatActivity {
+public class Signup_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        setContentView(R.layout.activity_signup_page);
     }
 }
