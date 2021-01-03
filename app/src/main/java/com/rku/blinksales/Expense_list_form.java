a77ab2691122cf2eafd26f13804c0cf3b4351e31
NSP User <dkoladia208@rku.ac.in>
2021-01-03 13:03:32 +0530

    Purchase List and Expense List Forms completed.

--- app/src/main/java/com/rku/blinksales/Expense_list_form.java
+++ app/src/main/java/com/rku/blinksales/Expense_list_form.java
@@ -1,14 +1,62 @@
 package com.rku.blinksales;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class Expense_list_form extends AppCompatActivity {
 
+    EditText id_exp_date,id_exp_to_whom,id_exp_amount,id_exp_note;
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense_list_form);
+
+        id_exp_date = findViewById(R.id.id_exp_date);
+        id_exp_to_whom = findViewById(R.id.id_exp_to_whom);
+        id_exp_amount = findViewById(R.id.id_exp_amount);
+        id_exp_note = findViewById(R.id.id_exp_note);
+
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker().setTheme(R.style.ThemeOverlay_MaterialComponents_MaterialCalendar);
+        MaterialDatePicker materialDatePicker =builder.build();
+        builder.setTitleText("SELECT DATE");
+
+        id_exp_date.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                materialDatePicker.show(getSupportFragmentManager(),"DATE PICKER");
+            }
+        });
+//        id_exp_date.setOnHoverListener(new View.OnHoverListener() {
+//            @Override
+//            public boolean onHover(View v, MotionEvent event) {
+//                materialDatePicker.show(getSupportFragmentManager(),"DATE PICKER");
+//                return true;
+//            }
+//        });
+
+
+
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                id_exp_date.setText(materialDatePicker.getHeaderText());
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/rku/blinksales/Purchase_list_form.java
+++ app/src/main/java/com/rku/blinksales/Purchase_list_form.java
@@ -3,12 +3,25 @@ package com.rku.blinksales;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Purchase_list_form extends AppCompatActivity {
+
+    EditText id_pur_lst_ven_name,id_pur_lst_amount,id_pur_lst_tax_amount,id_pur_lst_email,id_pur_lst_address;
+    Button id_pur_lst_btn_save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase_list_form);
+
+        id_pur_lst_ven_name = findViewById(R.id.id_pur_lst_ven_name);
+        id_pur_lst_amount = findViewById(R.id.id_pur_lst_amount);
+        id_pur_lst_tax_amount = findViewById(R.id.id_pur_lst_tax_amount);
+        id_pur_lst_email = findViewById(R.id.id_pur_lst_email);
+        id_pur_lst_address= findViewById(R.id.id_pur_lst_address);
+        id_pur_lst_btn_save = findViewById(R.id.id_pur_lst_btn_save);
+
     }
 }
