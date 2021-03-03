b4861763c9db2eee3c59b30126605b52f5c59edd
TheChiragRathod <crathod116@rku.ac.in>
2021-03-03 12:13:53 +0530

    validation added in Customers_form.java file

--- app/src/main/java/com/rku/blinksales/form/Customers_form.java
+++ app/src/main/java/com/rku/blinksales/form/Customers_form.java
@@ -6,11 +6,14 @@ import androidx.fragment.app.FragmentManager;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.rku.blinksales.R;
 import com.rku.blinksales.mainfragment.Customers;
@@ -44,6 +47,52 @@ public class Customers_form extends AppCompatActivity {
         id_back_arrow.setOnClickListener(v -> {
             onBackPressed();
         });
+
+    }
+
+    public void Save_Customer(View view)
+    {
+
+        String cus_name=id_cus_name.getText().toString().trim();
+        String cus_job=id_cus_job.getText().toString().trim();
+        String cus_contact=id_cus_contact.getText().toString().trim();
+        String cus_email=id_cus_email.getText().toString().trim();
+        String cusB_gst_num=id_cusB_gst_num.getText().toString().trim();
+        String cusB_address = id_cusB_address.getText().toString().trim();
+        String cusB_city=id_cusB_city.getText().toString().trim();
+        String cusB_region=id_cusB_region.getText().toString().trim();
+        String cusB_country=id_cusB_country.getText().toString().trim();
+        String cusB_pos_code=id_cusB_pos_code.getText().toString().trim();
+        String cusB_description = id_cusB_description.getText().toString().trim();
+
+        // validation code start here........
+        if(cus_name.isEmpty())
+            Toast.makeText(getApplicationContext(),"Enter name",Toast.LENGTH_SHORT).show();
+        else if(cus_job.isEmpty())
+            Toast.makeText(getApplicationContext(),"Enter job title",Toast.LENGTH_SHORT).show();
+        else if(cus_contact.isEmpty() || cus_contact.length()!=10)
+            Toast.makeText(getApplicationContext(),"You must enter 10 digits contact number",Toast.LENGTH_SHORT).show();
+        else if(!Patterns.EMAIL_ADDRESS.matcher(cus_email).matches())
+            Toast.makeText(getApplicationContext(),"Enter valid email id",Toast.LENGTH_SHORT).show();
+        else if (cusB_gst_num.isEmpty())
+            Toast.makeText(getApplicationContext(),"Enter GST number",Toast.LENGTH_SHORT).show();
+        else if(cusB_address.isEmpty())
+            Toast.makeText(getApplicationContext(),"Enter address",Toast.LENGTH_SHORT).show();
+        else if(cusB_country.isEmpty())
+            Toast.makeText(getApplicationContext(),"Enter country",Toast.LENGTH_SHORT).show();
+        else if(cusB_region.isEmpty())
+            Toast.makeText(getApplicationContext(),"Enter region",Toast.LENGTH_SHORT).show();
+        else if(cusB_city.isEmpty())
+            Toast.makeText(getApplicationContext(),"Enter city",Toast.LENGTH_SHORT).show();
+        else if(cusB_pos_code.isEmpty() || cusB_pos_code.length()!=6)
+            Toast.makeText(getApplicationContext(),"You must enter 6 digits postal code",Toast.LENGTH_SHORT).show();
+        else if(cusB_description.isEmpty())
+            Toast.makeText(getApplicationContext(),"Enter description",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(getApplicationContext(),"Success...",Toast.LENGTH_LONG).show();
+
+        // validation code finished here........
+
 
     }
 }
