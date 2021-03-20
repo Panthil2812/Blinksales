ee24aac870c73100b075a2989e1695d010fd0725
Panthil2812 <pmalaviya356@rku.ac.in>
2021-03-21 03:06:23 +0530

    some edit in Report

--- app/src/main/java/com/rku/blinksales/mainfragment/Report.java
+++ app/src/main/java/com/rku/blinksales/mainfragment/Report.java
@@ -14,8 +14,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.rku.blinksales.R;
+import com.rku.blinksales.Reports.Expense_report;
 import com.rku.blinksales.Reports.Purchase_report;
 import com.rku.blinksales.Reports.Sales_report;
+import com.rku.blinksales.Reports.Tax_report;
 import com.rku.blinksales.form.Customers_form;
 import com.rku.blinksales.form.Product_form;
 
@@ -27,28 +29,39 @@ public class Report  extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_report, container, false);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.nav_report);
-
-        txt_rpt_sales = getActivity().findViewById(R.id.txt_rpt_sales);
-        txt_rpt_purchase = getActivity().findViewById(R.id.txt_rpt_purchase);
-        txt_rpt_expense = getActivity().findViewById(R.id.txt_rpt_expense);
-        txt_rpt_customer = getActivity().findViewById(R.id.txt_rpt_customer);
-        txt_rpt_tax = getActivity().findViewById(R.id.txt_rpt_tax);
-        txt_rpt_vendor = getActivity().findViewById(R.id.txt_rpt_vendor);
-
-
         TextView id_weight = getActivity().findViewById(R.id.id_weight);
         ImageButton id_btn_refresh = getActivity().findViewById(R.id.id_btn_refresh);
         id_weight.setVisibility(View.GONE);
         id_btn_refresh.setVisibility(View.GONE);
+
+        //main page code
+        txt_rpt_sales = view.findViewById(R.id.txt_rpt_sales);
+        txt_rpt_purchase = view.findViewById(R.id.txt_rpt_purchase);
+        txt_rpt_expense = view.findViewById(R.id.txt_rpt_expense);
+        txt_rpt_customer = view.findViewById(R.id.txt_rpt_customer);
+        txt_rpt_tax = view.findViewById(R.id.txt_rpt_tax);
+        txt_rpt_vendor = view.findViewById(R.id.txt_rpt_vendor);
+
+        txt_rpt_sales.setOnClickListener(v -> {
+            startActivity(new Intent(getContext(),Sales_report.class));
+        });
+        txt_rpt_purchase.setOnClickListener(v -> {
+            startActivity(new Intent(getContext(),Purchase_report.class));
+        });
+        txt_rpt_expense.setOnClickListener(v -> {
+            startActivity(new Intent(getContext(), Expense_report.class));
+        });
+        txt_rpt_customer.setOnClickListener(v -> {
+//            startActivity(new Intent(getContext(),));
+        });
+        txt_rpt_tax.setOnClickListener(v -> {
+            startActivity(new Intent(getContext(), Tax_report.class));
+        });
+        txt_rpt_vendor.setOnClickListener(v -> {
+//            startActivity(new Intent(getContext(),));
+        });
+
         return view;
 
-
-//        txt_rpt_sales.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //Do something
-//
-//            }
-//        });
     }
 }
